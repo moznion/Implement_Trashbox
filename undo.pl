fbc48b65e72e6f415ab93a243264caac4c42525d
moznion <8d49h1akimew.ume@gmail.com>
2012-02-15 02:07:46 +0900

    I implemented function about undo the specified file.

--- undo.pl
+++ undo.pl
@@ -132,4 +132,47 @@ sub undoAll{
 }
 
 sub undoSpec{
+	#All files in trash box directory are revived.
+	foreach my $_directory( @ARGV ){
+		my $pathFileLocation = " <".$_directory."/.PATH";
+		unless( open PATH, $pathFileLocation ){
+			die "Cannot open .PATH file: $!\n";
+		}
+		my $targetUndoLocation;
+		while( <PATH> ){
+			chomp;
+			$targetUndoLocation = $_;
+		}
+		close PATH;
+
+		#Make directory if target directory is not exist
+		my @targetUndoLocationList = split /\//, $targetUndoLocation;
+		shift @targetUndoLocationList;  #Remove null element
+		my $directoryTest;
+		foreach( @targetUndoLocationList ){
+			$directoryTest .= "/" . $_;
+			unless( -e $directoryTest ){
+				mkdir "$directoryTest", 0755 or die "Cannot make directory: $!";
+			}
+		}
+
+		#Remove PATH information file
+		unlink $_directory."/.PATH";
+
+		#Undo
+		my @targetUndoFileList = glob "$_directory/* $_directory/.*";
+		my $targetUndoFile;
+		foreach( @targetUndoFileList ){
+			$targetUndoFile = $_;
+			s#^$_directory/##;
+			unless( $targetUndoFile =~ /\.$/ ){
+				rename $targetUndoFile, $targetUndoLocation."/".$_;
+				say "Undo : $targetUndoFile -> $targetUndoLocation/";
+			}
+		}
+
+		#Remove the newest directory
+		rmtree $_directory;
+
+	}
 }
